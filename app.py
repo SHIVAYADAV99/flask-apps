1e0ec4830f896dc49614c9382368b78946cf9b9f
shivayadav99 <shivayadav1u@gmail.com>
2025-11-28 04:39:34 +0000

    app update

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def home():
-    return "Flask + Gunicorn + Nginx is working!"
+    return "Flask2.0 + Gunicorn + Nginx is working!"
 
 if __name__ == "__main__":
     app.run()
